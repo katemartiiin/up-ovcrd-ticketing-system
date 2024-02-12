11810d115d5631a65a8fbbb6492e254bf86b55f8
Kate Martin <katejaneen23@gmail.com>
2024-02-12 16:27:47 +0800

    Initial commit

--- node_modules/vue3-highcharts/src/index.js
+++
@@ -1,9 +0,0 @@
-import Vue3Highcharts from './vue3-highcharts';
-
-const install = (app) => {
-  app.component(Vue3Highcharts.name, Vue3Highcharts);
-};
-
-Vue3Highcharts.install = install;
-
-export default Vue3Highcharts;
